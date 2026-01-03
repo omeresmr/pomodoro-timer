ba2a5bd69cf925552a1aa6ac961f30d451d8057a
omeresmr <bxnger36@gmail.com>
2026-01-03 21:08:50 +0300

    implement navigation for logo & darkmode logic

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,10 +1,11 @@
 import { Timer } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
-    <a className="logo" href="./index.html">
+    <Link to="/" className="logo">
       <Timer className="logo-gradient logo-icon" />
       <p className="logo-gradient logo-text">FocusFlow</p>
-    </a>
+    </Link>
   );
 }
